443072ba9f37cc04530c79ae6e4a36aa3a1fd928
daferarte <daferarte@gmail.com>
2023-03-23 08:12:47 -0500

    Caso de estudio empleado

---
+++ Empleado/src/Empleado/Fecha.java
@@ -0,0 +1,19 @@
+package Empleado;
+
+public class Fecha {
+	private int dia;
+	private int mes;
+	private int anio;
+	
+	public int DarAnio(){
+		return this.anio;
+	}
+	
+	public int DarMes(){
+		return this.mes;
+	}
+	
+	public int DarDia(){
+		return this.dia;
+	}
+}
